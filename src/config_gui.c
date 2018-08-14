e3d7e4c76aee9f9e2de94deae22785f1405c7243
Konstantin <ria.freelander@gmail.com>
2018-08-14 20:08:07 +0300

    config_gui.c: remove unneded scripts.

--- src/config_gui.c
+++ src/config_gui.c
@@ -30,22 +30,6 @@
 
 /**** GUI initially created using glade-2 ****/
 
-/* Use the gtk_button_new_with_mnemonic() function for text-based
-   push buttons */
-/* Use "#define gtk_button_new_with_mnemonic(x) gtk_button_new()"
-   for color-filled buttons */
-
-#define gtk_button_new_with_mnemonic(x) gtk_button_new()
-
-#define GLADE_HOOKUP_OBJECT(component, widget, name)                                               \
-	g_object_set_data_full(G_OBJECT(component),                                                \
-	                       name,                                                               \
-	                       gtk_widget_ref(widget),                                             \
-	                       (GDestroyNotify)gtk_widget_unref)
-
-#define GLADE_HOOKUP_OBJECT_NO_REF(component, widget, name)                                        \
-	g_object_set_data(G_OBJECT(component), name, widget)
-
 int genmon_CreateConfigGUI(GtkWidget *vbox1, struct gui_t *p_poGUI)
 {
 	GtkWidget *table1;
