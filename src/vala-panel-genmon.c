8126bf55f1a341204e092b4491be80303712e140
Konstantin <ria.freelander@gmail.com>
2019-04-26 01:02:30 +0300

    vala-panel: use applet as extension point implementor

--- src/vala-panel-genmon.c
+++ src/vala-panel-genmon.c
@@ -125,45 +125,6 @@ static void genmon_applet_class_finalize(GenMonAppletClass *klass)
 }
 
 /*
- * Plugin functions
- */
-
-struct _GenMonPlugin
-{
-	ValaPanelAppletPlugin parent;
-};
-
-G_DEFINE_DYNAMIC_TYPE(GenMonPlugin, genmon_plugin, vala_panel_applet_plugin_get_type())
-
-static ValaPanelApplet *genmon_plugin_get_applet_widget(ValaPanelAppletPlugin *base,
-                                                        ValaPanelToplevel *toplevel,
-                                                        GSettings *settings, const char *uuid)
-{
-	g_return_val_if_fail(toplevel != NULL, NULL);
-	g_return_val_if_fail(uuid != NULL, NULL);
-
-	return VALA_PANEL_APPLET(genmon_applet_new(toplevel, settings, uuid));
-}
-
-GenMonPlugin *genmon_plugin_new(GType object_type)
-{
-	return GENMON_PLUGIN(vala_panel_applet_plugin_construct(genmon_applet_get_type()));
-}
-
-static void genmon_plugin_class_init(GenMonPluginClass *klass)
-{
-	((ValaPanelAppletPluginClass *)klass)->get_applet_widget = genmon_plugin_get_applet_widget;
-}
-
-static void genmon_plugin_init(GenMonPlugin *self)
-{
-}
-
-static void genmon_plugin_class_finalize(GenMonPluginClass *klass)
-{
-}
-
-/*
  * IO Module functions
  */
 
@@ -172,11 +133,10 @@ void g_io_genmon_load(GTypeModule *module)
 	g_return_if_fail(module != NULL);
 
 	genmon_applet_register_type(module);
-	genmon_plugin_register_type(module);
 
 	g_type_module_use(module);
 	g_io_extension_point_implement(VALA_PANEL_APPLET_EXTENSION_POINT,
-	                               genmon_plugin_get_type(),
+	                               genmon_applet_get_type(),
 	                               "org.valapanel.genmon",
 	                               10);
 }
