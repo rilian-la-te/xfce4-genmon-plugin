88c7157b157c7da98a0e05465870f80871ee9ecf
Konstantin <ria.freelander@gmail.com>
2019-04-28 00:56:42 +0300

    vala-panel: do not use a module.

--- src/vala-panel-genmon.h
+++ src/vala-panel-genmon.h
@@ -25,8 +25,6 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE(GenMonApplet, genmon_applet, GENMON, APPLET, ValaPanelApplet)
 
-G_DECLARE_FINAL_TYPE(GenMonPlugin, genmon_plugin, GENMON, PLUGIN, ValaPanelAppletPlugin)
-
 G_END_DECLS
 
 #endif // VALAPANELGENMON_H
